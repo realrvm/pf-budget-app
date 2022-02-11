2229242fce524f5932dade3f1689293f6afcbc60
Roman <roman.mikaelyan@fogstream.ru>
2022-02-11 08:24:05 +0300

    стилизация шага 3

--- src/pages/login-form/components/FileInput.tsx
+++ src/pages/login-form/components/FileInput.tsx
@@ -8,7 +8,7 @@ import { RootState } from "store/store";
 import { addFile, deleteFile } from "store/stepTreeReducer";
 import { getRandomId } from "utils/utils";
 
-const Input = ({ name, control }: { name: string; control: any }) => {
+const FileInput = ({ name, control }: { name: string; control: any }) => {
     const dispatch = useDispatch();
     const store = useSelector((state: RootState) => state.stepThreeReducer);
 
@@ -22,7 +22,7 @@ const Input = ({ name, control }: { name: string; control: any }) => {
         [dispatch]
     );
 
-    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+    const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({ onDrop });
 
     const deleteFileFromStore = (id: string) => {
         dispatch(deleteFile({ id }));
@@ -35,14 +35,24 @@ const Input = ({ name, control }: { name: string; control: any }) => {
             defaultValue={[]}
             render={() => (
                 <>
-                    <div {...getRootProps({ className: "dropzone" })}>
+                    <div
+                        {...getRootProps({
+                            className: `dropzone ${isDragAccept && "dropzoneAccept"} ${
+                                isDragReject && "dropzoneReject"
+                            }`,
+                        })}
+                    >
                         <input {...getInputProps()} />
-                        <p>Загрузка файлов</p>
+                        {isDragActive ? (
+                            <p className="dropzone__text">Можно отпускать!</p>
+                        ) : (
+                            <p className="dropzone__text">Перетащите файлы сюда или клик для выбора</p>
+                        )}
                     </div>
                     <div>
                         {store.map((e) => {
                             return (
-                                <div key={e.id} style={{ display: "flex" }}>
+                                <div key={e.id} className="dropzone__put">
                                     <p>{e.name}</p>
                                     <button type="button" onClick={() => deleteFileFromStore(e.id)}>
                                         &#10008;
@@ -57,4 +67,4 @@ const Input = ({ name, control }: { name: string; control: any }) => {
     );
 };
 
-export default Input;
+export default FileInput;
--- src/pages/login-form/components/StepThree.tsx
+++ src/pages/login-form/components/StepThree.tsx
@@ -1,14 +1,32 @@
-import { Input } from ".";
+import { FileInput } from ".";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const StepThree = () => {
     const { control } = useForm();
+    const navigate = useNavigate();
+
+    const onSubmit = () => {
+        navigate("/login/form/result");
+    };
+
+    const illBeBack = () => {
+        navigate("/login-form/steptwo");
+    };
 
     return (
-        <form>
-            <Input name={`name`} control={control} />
-            <button type="submit">Загрузка</button>
-        </form>
+        <div className="login-form__wrapper">
+            <h2>Шаг 3</h2>
+            <form onSubmit={onSubmit}>
+                <FileInput name={`name`} control={control} />
+                <button type="button" onClick={illBeBack} className="login-form__btn">
+                    Назад
+                </button>
+                <button type="submit" className="login-form__btn">
+                    Далее
+                </button>
+            </form>
+        </div>
     );
 };
 
